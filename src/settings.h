a29afd932945b4baa1ef10c27916e4e0cd57506e
smallker <wahyu.smallker@gmail.com>
2020-10-23 19:28:41 +0700

    finish

---
+++ src/settings.h
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+const char *ssid = "y";
+const char *password = "Kenari.123";
+
+#define CHAT_ID "1123810574"
+#define BOTtoken "1096181817:AAFAdvG8exQgYiF6q6s3g2pWGwNBwLsUHa4"
